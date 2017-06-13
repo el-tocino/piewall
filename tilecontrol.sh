eae231609563e101a6c793669e5c97e9c17d0b7b
el-tocino <email+github@aforkinthetoad.com>
2017-06-12 21:43:54 -0500

    more granular

--- tilecontrol.sh
+++ tilecontrol.sh
@@ -3,8 +3,8 @@
 # control the player (startplayer.sh) on tiles
 #
 
-if [[ $# -lt 2 ]] ; then
-    echo "Usage: $0 action [options].
+if [[ $# -lt 3 ]] ; then
+    echo "Usage: $0 WALL action [options].
            start PIIP
            stop PIIP
            restart PIIP
@@ -13,20 +13,27 @@ if [[ $# -lt 2 ]] ; then
            assign PIIP TILEID
 "
 fi
-
-IPPREFIX='YOURIPHERE'
-SSHCON="ssh -o ConnectTimeout=10 pi@${IPPREFIX}"
-STARTPLYR="screen -mdS piwall /home/pi/startplayer.sh"
-STOPPLYR="screen -X -S piwall quit"
-IDPLYR="export DISPLAY=:0.0 ; leafpad /home/pi/playerid.txt"
+PIWALL="$1"
+shift
+SSHUSER="pi"
+SSHOPT="-o ConnectTimeout=10"
+IPP="192.168.16.10"
+STARTPLYR="'screen -mdS ${PIWALL} /home/pi/startplayer.sh'"
+STOPPLYR="'screen -X -S ${PIWALL} quit'"
+IDPLYR="'export DISPLAY=:0.0 ; leafpad /home/pi/playerid.txt'"
 STOPID="sleep 60 && killall leafpad"
 RBC="sudo reboot"
 PACT="${1}" 
 TILECMD1="sed -i /home/pi/.pitile -e 's/^id*/id="
 shift
 
+sshtile() {
+echo "ssh ${SSHOPT} ${SSHUSER}@${IPP}$*"
+ssh "${SSHOPT}"  "${SSHUSER}@${IPP}$*"
+}
+
 startplayer() {
-if "${SSHCON}${1}" "${STARTPLYR}" ; then
+if sshtile "${1}" "${STARTPLYR}" ; then
     echo "Started player in screen on Pi${1}."        
 else 
     echo "Failed to start player on pi${1}!"
@@ -36,18 +43,22 @@ fi
 }
 
 stopplayer() {
-if "${SSHCON}${1}" "${STOPPLYR}" ; then
-    echo "Stopped screen on Pi${1}."
+if sshtile "${1}" "${STOPPLYR}" ; then
+  echo "Stopped screen on Pi${1}."
 else
+  if sshtile "${1}" "killall screen" ; then 
+     echo "Stopped all screens on Pi${1}."
+  else
     echo "Failed to stop player on Pi${1}!"
     return 162
+  fi
 fi
 }
 
 idplayer() {
-if "${SSHCON}${1}" "echo ${1} ${1} ${1} > /home/pi/playerid.txt" ; then
-    if "${SSHCON}${1}" "${IDPLYR}" ; then
-         "${SSHCON}${1}" "${STOPID}" 
+if sshtile "${1}" "echo ${1} ${1} ${1} > /home/pi/playerid.txt" ; then
+    if sshtile "${1}" "${IDPLYR}" ; then
+         sshtile "${1}" "${STOPID}" 
     else
          echo "Unable to start leafpad on Pi${1}!"
          return 164
@@ -60,7 +71,7 @@ rm  /home/pi/playerid.txt
 }
 
 assign() {
-if "${SSHCON}${1}" "${TILECMD1}""$2" ; then
+if sshtile "${1}" "${TILECMD1}""$2" ; then
     echo "Assigned tile $1 the ID $2!"
 else
     echo "Unable to assign tile $1 the ID $2!"
@@ -70,11 +81,11 @@ fi
 }
 
 rebootpi() {
-"${SSHCON}${1}" "${RBC}"
+sshtile "${1}" "${RBC}"
 }
 
 while [[ $# -gt 0 ]] ; do
-    if [[ "${1}" =~  =~ ^-?[0-9]+$ ]] ; then
+    if [[ "${1}" =~ ^(1|2|3|4|5|6)$ ]]; then
         PLYR="${1}"
         shift;
         case "${PACT}" in
@@ -87,7 +98,7 @@ while [[ $# -gt 0 ]] ; do
             *) echo "invalid argument ${PLYR}" ;;
         esac
     else
-        echo "${1} needs to be a number!"
+        echo "${1} needs to be a number from 1-6!"
         shift;
     fi
 done
